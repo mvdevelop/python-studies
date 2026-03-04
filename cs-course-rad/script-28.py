2d65a204cefe7300154a6e1ac3e295e46c69c9df
mvdevelop <marcosvmdilly@gmail.com>
2026-03-04 08:24:41 -0300

    feat: working on cs-course-rad

---
+++ cs-course-rad/script-28.py
@@ -0,0 +1,14 @@
+
+import os
+
+try: 
+    os.mkdir("meu_diretorio")
+    print("Diretório criado !")
+except PermissionError as erro: 
+    print("Sem permissão para criar diretório")
+    print("Descrição", erro)
+except FileExistsError as erro: 
+    print("Diretório já existe")
+    print("Descrição", erro)
+
+print("Término do programa")
